0c0643bbcee0e77ded31f1b2e08e1349ccf076c5
LuaneSopelsa <luane.sopelsa@edu.udesc.br>
2024-12-06 16:04:48 -0300

    Ajuste do formulário de inscrição

--- back-end/processa_form.php
+++ back-end/processa_form.php
@@ -5,9 +5,9 @@ $idade = $_GET['idade'];
 
 echo '<h1>Dados do Usuário</hz>';
 echo '<hr>';
-echo "<p>O usuário $nome tem $idade amos.</p>";
+echo "<p>O usuário $nome tem $idade anos.</p>";
 
-echo '<a href="https://localhost/udesc/projeto-web/index.html"><button>Home Page</button></a>';
-echo '<button type="button" onclick="javascript:history.bach()">Voltar</button>';
-echo '<button onclick="window.location.href=\'https:\\\\www.udesc.com.br\'">Vai para Udesc</button>';
+echo '<a href="../index.html"><button>Home Page</button></a>';
+echo '<button type="button" onclick="javascript:history.back()">Voltar</button>';
+echo '<button onclick="window.location.href=\'https://www.udesc.br\'">Vai para Udesc</button>';
 ?>
